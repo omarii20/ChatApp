163e5687a45e94ef52e29ef0fcf3277e5bb2af03
Mohamed Omarii <mohamed.44301hotmail.com>
2024-11-10 14:04:05 +0200

    removed images, and stay on default user image

--- src/utils/images.tsx
+++ src/utils/images.tsx
@@ -2,7 +2,7 @@
 
 export const images: Record<string, any> = {
     default: require('../images/default.jpeg'),
-    user1: require('../images/user1.jpg'),
-    user2: require('../images/user2.jpeg')
+    // user1: require('../images/user1.jpg'),
+    // user2: require('../images/user2.jpeg')
   };
   
